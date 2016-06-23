4fa433debb697af0ee1004fa5f8918b48a9ddb8a
Sasidhar Reddy Swarna Venkata Naga <Sasidhar Reddy Swarna Venkata Naga>
2016-06-23 18:25:04 -0400

    first commit and custom service created

---
+++ app.js
@@ -0,0 +1,35 @@
+// Module
+var weatherApp = angular.module('weatherApp', ['ngRoute','ngResource']);
+
+
+//router
+weatherApp.config(function ($routeProvider){
+	$routeProvider
+
+	.when('/',{
+		templateUrl: 'pages/home.html',
+		controller: 'homeController'
+	})
+
+	.when('/forecast',{
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+});
+
+//services
+weatherApp.service('cityService', function(){
+	this.city = "New York, NY";
+});
+//Controllers
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
+	$scope.$watch('city', function(){
+		cityService.city = $scope.city;
+	})
+
+}]);
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
+
+}]);
